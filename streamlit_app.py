862a0f4f6f62031dac57908533a328b4274c5b9d
chauser6 <carlena.hauser1108@gmail.com>
2024-10-07 08:52:25 -0400

    add a multi-select for Sub_Category

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,6 +38,12 @@ df_category = df[df['Category'] == category]
 
 
 st.write("### (2) add a multi-select for Sub_Category *in the selected Category (1)* (https://docs.streamlit.io/library/api-reference/widgets/st.multiselect)")
+sub_categories = df_category['Sub-Category'].unique()
+selected_sub_categories = st.multiselect("Select Sub-Category", sub_categories, default=sub_categories)
+# Filter the DataFrame based on selected Sub-Categories
+df_filtered = df_category[df_category['Sub-Category'].isin(selected_sub_categories)]
+
+
 st.write("### (3) show a line chart of sales for the selected items in (2)")
 st.write("### (4) show three metrics (https://docs.streamlit.io/library/api-reference/data/st.metric) for the selected items in (2): total sales, total profit, and overall profit margin (%)")
 st.write("### (5) use the delta option in the overall profit margin metric to show the difference between the overall average profit margin (all products across all categories)")
